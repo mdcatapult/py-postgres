b2da89285ff00bd7d2116738632b00a9fc3b517f
Nick Etherington <nick.etherington@md.catapult.org.uk>
2022-06-21 15:59:32 +0100

    Tidy

--- src/klein_postgres/postgres.py
+++ src/klein_postgres/postgres.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 #  Copyright 2022 Medicines Discovery Catapult
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,6 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+# -*- coding: utf-8 -*-
 import psycopg2.extras
 
 from .connect import PostgresConnection
